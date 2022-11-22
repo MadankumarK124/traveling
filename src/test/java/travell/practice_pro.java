c01de4ff4a7523e6d5ad0267ef1c9e84966dffb7
MadankumarK124 <118715280+MadankumarK124@users.noreply.github.com>
2022-11-22 16:58:18 +0530

    Update practice_pro.java

--- src/test/java/travell/practice_pro.java
+++ src/test/java/travell/practice_pro.java
@@ -4,8 +4,7 @@ import org.testng.reporters.jq.Main;
 
 public class practice_pro {
 	public static void main(String[] args) {
-		System.out.println("insert");
-		System.out.println("Disha patni");
+
 		System.out.println("Kiara advani");
 	}
 
